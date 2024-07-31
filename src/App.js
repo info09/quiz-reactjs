92974f4bb4a02df361e1dd3d7eddd647d9df5a62
huytq <huytq3103@gmail.com>
2024-07-31 21:48:10 +0700

    Active link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 const App = () => {
